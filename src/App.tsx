87ad1fbf6450ce8208b19550c9e43f1f8a272fec
yatochkaaa <jaffplay@gmail.com>
2023-03-31 11:17:36 +0300

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import AppRouter from "./components/AppRouter";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <AppRouter />
     </BrowserRouter>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { ROUTE } from "../utils/consts";
+
 const NavBar: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>NavBar</div>
+    <Navbar collapseOnSelect  bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand className="me-5" href={ROUTE.SHOP}>SheepFish</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => navigate(ROUTE.SHOP)}>Товары</Nav.Link>
+          </Nav>
+          <Button
+              variant="outline-light"
+              onClick={() => navigate(ROUTE.ADMIN)}
+            >
+              Админ панель
+            </Button>
+      </Container>
+    </Navbar>
   );
-}
+};
 
 export default NavBar;
